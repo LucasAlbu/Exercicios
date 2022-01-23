03f9bc1cdf3fb727d506ffc69f2d019d070a1946
LucasAlbu <78665665+LucasAlbu@users.noreply.github.com>
2022-01-23 18:35:05 -0300

    task 1 completed

--- sistemaDeNotas.js
+++ sistemaDeNotas.js
@@ -1,5 +1,18 @@
 let nota = 90;
+let grade;
 
-if (nota>=90) {
-    console.log("A");
+function escrever(grade) {
+    console.log(grade);
+}
+
+if (nota >= 90) {
+    escrever("A");
+} else if (nota <= 89 || nota >= 80) {
+    escrever("B");
+} else if (nota <= 79 || nota >= 70) {
+    escrever('C');
+} else if (nota <= 69 || nota >= 60) {
+    escrever('D');
+} else if (nota < 60) {
+    escrever('F');
 }
